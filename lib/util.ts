4d73e9ad0b42f65b4c59ec3226fac639a5b25b31
ahmed-ae <Ahmed.Okour@united.com>
2024-09-09 14:45:02 -0500

    Update the chat flow to allow incremental updates to the same code returned

--- lib/util.ts
+++ lib/util.ts
@@ -1,3 +1,5 @@
+import { promptMessages } from "./PromptMessagesTemplates";
+
 export interface Message {
   role: string;
   content: string;
@@ -144,166 +146,69 @@ export function generateCodeConversionPrompt(
 
 export function generateImage2CodePrompt(
   customInstructions: string,
-  additionalInstructions: string
+  additionalInstructions: string,
+  messageHistory: string[],
+  previouslyGeneratedCode: string
 ): Message[] {
   let systemMessage = "";
-  let userPrompt = "";
-  let userCustomInstructions = "";
-  if (customInstructions && customInstructions != "") {
-    userCustomInstructions = customInstructions + " \n ";
+  let userPrompt1 = promptMessages.userMessageImagePrompt;
+  let userPrompt2 = "";
+  let assistantMessage = "";
+
+  //If there is previouslyGeneratedCode, we need to create user prompt 1 with custom instructions and message history if available
+  if (previouslyGeneratedCode && previouslyGeneratedCode != "") {
+    if (
+      (customInstructions && customInstructions != "") ||
+      (messageHistory && messageHistory.length > 0)
+    ) {
+      userPrompt1 +=
+        "\n Also follow custom instructions delimited by triple backticks, which are directly provided by the user uploading the image ```" +
+        customInstructions +
+        (messageHistory ? " \n " + messageHistory.join(" \n ") : "") +
+        " ```";
+    }
+  } else {
+    //If there is no previouslyGeneratedCode, we need to create user prompt 1 with custom instructions and additional instructions if available
+    if (
+      (customInstructions && customInstructions != "") ||
+      (additionalInstructions && additionalInstructions != "")
+    ) {
+      userPrompt1 +=
+        "\n Also follow custom instructions delimited by triple backticks, which are directly provided by the user uploading the image ```" +
+        customInstructions +
+        (additionalInstructions ? " \n " + additionalInstructions : "") +
+        " ```";
+    }
   }
-  if (additionalInstructions && additionalInstructions != "") {
-    userCustomInstructions += additionalInstructions + " \n ";
+
+  systemMessage = promptMessages.systemMessageImagePrompt;
+  if (
+    previouslyGeneratedCode &&
+    previouslyGeneratedCode != "" &&
+    additionalInstructions &&
+    additionalInstructions != ""
+  ) {
+    assistantMessage = previouslyGeneratedCode;
+    userPrompt2 = additionalInstructions;
   }
-  userPrompt = `Convert the attached image into Sitecore JSS NextJs (TypeScript) Component and follow these rules:
-    - make sure to Implement fully responsive design, Ensure the design is fluid and adjusts smoothly between breakpoints for different screen sizes    
-    - Analyze the provided image in detail, breaking down its visual elements, layout, color scheme, and typography.
-    - Use a cohesive color scheme that matches the branding of the attached image, including specific colors for backgrounds, text, and interactive elements like links and tags.
-    - Ensure the code implementation matches the visual design as closely as possible.
-    - Make sure to keep the colors of buttons, fonts and other interactive elements the same as in the attached image
 
-  `;
+  if (userPrompt2 && assistantMessage) {
+    const messages: Message[] = [
+      { role: "system", content: systemMessage },
+      { role: "user", content: userPrompt1 },
+      { role: "assistant", content: assistantMessage },
+      { role: "user", content: userPrompt2 },
+    ];
 
-  if (userCustomInstructions && userCustomInstructions != "") {
-    userPrompt +=
-      "\n Also follow custom instructions delimited by triple backticks, which are directly provided by the user uploading the image ```" +
-      userCustomInstructions +
-      " ```";
+    return messages;
+  } else {
+    const messages: Message[] = [
+      { role: "system", content: systemMessage },
+      { role: "user", content: userPrompt1 },
+    ];
+
+    return messages;
   }
-  systemMessage = `Act like a react code generator expert, where you convert images into Sitecore JSS components written in react/typescript 
-      
-    we want to generate 2 react components, For the First component you must follow these rules:
-      - it should be agnostic of @sitecore-jss/sitecore-jss-nextjs' library, and should assume any prop in field property can any JSX element, this component should render the entire html
-      - In case the attached image includes multiple cards , split it into two components,a parent/container component that will hold the multiple individual card/column components
-      - extract the component props from the attached image and assign the right property type for each prop
-
-    For the first component, follow these rules for design,styling and structure:
-      - If user does not specifically ask for specific styling library to use then use Make sure to Use Tailwind CSS classes for styling. If any styles can't be achieved with Tailwind, include custom CSS as needed, otherwise use the library that the user asked for
-      - regardless of what design/styling library the user ask for, make sure to Implement fully responsive design, Ensure the design is fluid and adjusts smoothly between breakpoints for different screen sizes
-      - if user asks for CSS modules, Please create a responsive CSS module that uses a grid layout for larger screens and stacks cards vertically on smaller screens. Include media queries for different screen sizes.
-      - If user asks for styled components, include the code for styled components library in first component
-      - Analyze the provided image in detail, breaking down its visual elements, layout, color scheme, and typography.
-      - Implement a clear hierarchy for text elements, with distinct styles for the main title, card titles, and links
-      - Use a cohesive color scheme that matches the branding of the attached image, including specific colors for backgrounds, text, and interactive elements like links and tags.
-      - Ensure the code implementation matches the visual design as closely as possible.
-      - Make sure to keep the colors of buttons, fonts and other interactive elements the same as in the attached image
-      - Generate semantic HTML5 markup that reflects the structure and content hierarchy of the design.
-      - Ensure that interactive elements (e.g., navigation menus, buttons) are usable on both touch and non-touch devices.
-      - Optimize the code for performance, keeping it clean, well-commented, and following best practices for web accessibility (WCAG guidelines).
-      - Make sure fonts and background colors are exactly matching what is in the attached image
-      - Make the component self-contained for easy preview
-      - create mock data object (named mockData) that matches what in the attached image and use that mock data to preview the first component, if mock data contains images that can be rendered as SVG, then generate SVG elements yourself and don't use any library, otherwise replace the image url with url from picsum.photos or any available CDN for images
-      - in mockdata make sure NOT to use single quotes anywhere
-      - If  the attached image is a carosuel, add few slides with lorem Epsom mock data and make sure to match the style and design of the carousel arrows and rotate dots
-      - if the attached image contains Card images, Ensure that card images are responsive and maintain their aspect ratio while fitting within the cards, Design the cards to have consistent sizing and spacing, with the content (tag, title, link) properly aligned within each card..
-      - preview component should always have the name PreviewComponent, and don't export the component, just ensure the component starts with : const PreviewComponent: React.FC
-
-    Now For the second component, follow these rules:
-      - Second component which is a wapper around the first component, this component is @sitecore-jss/sitecore-jss-nextjs' aware, and receive all needed preps for @sitecore-jss/sitecore-jss-nextjs'.
-      - follow this as an example -> type ComponentNameProps = ComponentProps & {fields: {imagefield: ImageField;textfield: Field<string>;linkfield: LinkField;datefield: DateField} };
-      - include this import : import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs'
-      - for every component, define  ComponentProps as type ComponentProps = { rendering: ComponentRendering; params: ComponentParams; }
-      - use (props: ComponentNameProps): JSX.Element  instead of React.FC<props>
-     
-
-      Most importantly,  your output must only contain converted code with optional CSS module if the user ask for it, don't include any explanations in your responses
-      So your output MUST be 
-      (Optional CSS module - only if user asked for CSS modules, NOT for styled components) prefixed with a comment /*start css module*/ and ended with /*end css module*/ also add a comment after the prefix with the name of the file, for example /*filename - component.module.css*/
-      (First Component) prefixed with a comment /*start first component*/ and ended with /*end first component*/ also add a comment after the prefix with the name of the file, for example /*filename - componentName.tsx*/
-      (Second Component) prefixed with a comment /*start second component*/ and ended with /*end second component*/ also add a comment after the prefix with the name of the file, for example /*filename - SitecoreWrapperComponent.tsx*/
-
-      Here is an example of a Hero Banner component that you can use as a reference:
-      //First component : FED Component (Sitecore Agnostic)
-      import React from 'react';
-
-      interface HeroProps {
-        fields: {
-          SlideImage: JSX.Element;
-          SlideCategory: JSX.Element;
-          SlideTitle: JSX.Element;
-          SlideContent: JSX.Element;
-          SlideLink: JSX.Element;
-        };
-      }
-
-      const HeroComponent: React.FC<HeroProps> = ({ fields }) => (
-        <div>
-          <div>
-            <div>
-              {fields.SlideImage}
-            </div>
-            <div>
-              <div>
-                <div>
-                  {fields.SlideCategory}
-                </div>
-                <h2>
-                  {fields.SlideTitle}
-                </h2>
-                <div>
-                  {fields.SlideContent}
-                </div>
-                {fields.SlideLink}
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-
-      export default HeroComponent;
-      //End of first component 
-
-
-      //Start of second component, Sitecore-jss Hero Wrapper Component
-      import {
-        Field,
-        Image,
-        ImageField,
-        LinkField,
-        RichText,
-        Text,
-        withDatasourceCheck,
-      } from '@sitecore-jss/sitecore-jss-nextjs';
-      import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
-      import { Link } from '@sitecore-jss/sitecore-jss-react';
-      import HeroComponent from './HeroComponent';
-
-      type ComponentProps = { rendering: ComponentRendering; params: ComponentParams };
-
-      type HeroProps = ComponentProps & {
-        id: string;
-        fields: {
-          SlideTitle: Field<string>;
-          SlideImage: ImageField;
-          SlideLink: LinkField;
-          SlideCategory: Field<string>;
-          SlidePublishDate: Field<string>;
-          SlideContent: Field<string>;
-        };
-      };
-
-      const SitecoreHeroWrapper = (props: HeroProps): JSX.Element => (
-        <HeroComponent
-          fields={{
-            SlideImage: <Image field={props.fields.SlideImage} />,
-            SlideCategory: <Text field={props.fields.SlideCategory} />,
-            SlideTitle: <Text field={props.fields.SlideTitle} />,
-            SlideContent: <RichText field={props.fields.SlideContent} />,
-            SlideLink: <Link field={props.fields.SlideLink} />,
-          }}
-        />
-      );
-
-      export default withDatasourceCheck()<HeroProps>(SitecoreHeroWrapper);
-      
-    `;
-
-  const messages: Message[] = [
-    { role: "user", content: userPrompt },
-    { role: "system", content: systemMessage },
-  ];
-
-  return messages;
 }
 
 export function extractCodeSection(
