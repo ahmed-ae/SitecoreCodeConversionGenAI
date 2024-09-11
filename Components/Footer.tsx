c85a159ed0c0d33f86ad8b943b4468f48f5ee5b1
ahmed-ae <Ahmed.Okour@united.com>
2024-09-11 10:48:06 -0500

    github logo

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-
+import { Github } from "lucide-react"; // Import the Github icon
 const Footer: React.FC = () => {
   return (
     <footer className="text-center mt-8 sm:mt-12 text-gray-400">
       <p className="flex flex-col sm:flex-row justify-center items-center space-y-2 sm:space-y-0 sm:space-x-2">
         <span className="hidden sm:inline">|</span>
+
         <a
           href="https://github.com/ahmed-ae/SitecoreCodeConversionGenAI"
           target="_blank"
           rel="noopener noreferrer"
           className="text-red-500 hover:text-red-400 transition duration-300"
         >
-          Github
+          <Github size={20} className="mr-2" />
         </a>
         <span className="hidden sm:inline">|</span>
       </p>
