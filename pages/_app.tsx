d128d7d49dfee829e7ef1142efb80f6078c0fdb5
ahmed-ae <Ahmed.Okour@united.com>
2024-09-30 09:55:25 -0500

    don't initialize posthog if is not set

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,9 +8,9 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { useEffect } from "react";
 
-if (typeof window !== "undefined") {
-  // checks that we are client-side
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  // checks that we are client-side and PostHog key is set
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host:
       process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
